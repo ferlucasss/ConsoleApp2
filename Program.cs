f5be518d050bc58c9b6be31534329f954df8c501
Lucas Fernandez <lucasfernandezepe@gmail.com>
2022-10-23 16:16:49 -0300

    primer commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] lista = new int[3];
+lista[0] = 1;
+lista[1] = 2;
+lista[2] = 3;
+
+
+foreach (int i in lista)
+{
+    Console.WriteLine(i);
+}
